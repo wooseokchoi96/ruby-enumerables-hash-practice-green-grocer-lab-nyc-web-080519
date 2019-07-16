61ba02c20e6f80017e2400b458cf863b324a68be
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 16:23:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,9 @@
 def consolidate_cart(cart)
   count = Hash.new(0)
-  cart.each{|key,value| count(key)
+  cart.each{|key,value| 
+    count(key) += 1
+    
+  }
 end
 
 def apply_coupons(cart, coupons)
